6614f4cdb82c177617dc0ba18a09a8305ff07b8b
Bosko Pajkic <bpajkic@ryerson.ca>
2023-11-15 19:44:49 -0500

    Initial Commit

---
+++ src/MainClass.java
@@ -0,0 +1,26 @@
+
+import java.io.*;
+import java.util.*;
+
+
+//import java.util.Scanner;
+
+/**
+ * CPS 510 Assignment - Payroll Management System
+ * @author Bosko Pajkic, Noah Coutlee, Clay Barnett
+ */
+public class MainClass {
+    
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+        
+//        Put DataBase Code Here? Put in 
+        
+        LoginScreen loginScreen = new LoginScreen();
+        loginScreen.show();
+        
+
+    }
+}
